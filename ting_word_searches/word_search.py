bb12508c315eda3c39fe16de51a1e9300f1e4c45
johntvale <johntvale@gmail.com>
2022-07-18 21:43:32 -0400

    Requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    word_lower = word.lower()
+    for file in instance._data:
+        lines = []
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            line_lower = line.lower()
+            if re.search(word_lower, line_lower):
+                lines.append(
+                    {"linha": i + 1}
+                )
+        if len(lines) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": lines
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
