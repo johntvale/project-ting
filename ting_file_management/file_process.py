2a163b29f23d32a05aec1e362c2b86b0462de303
johntvale <johntvale@gmail.com>
2022-07-18 20:34:37 -0400

    Requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -27,4 +28,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        in_queue = instance.search(position)
+        print(in_queue)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
