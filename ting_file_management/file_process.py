ae869d61844970624ffd3b39897a1b0e5a520fa0
johntvale <johntvale@gmail.com>
2022-07-18 20:23:30 -0400

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_already_exist = False
+    item_to_process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+    for item in instance._data:
+        if item["nome_do_arquivo"] == path_file:
+            file_already_exist = True
+            break
+    if not file_already_exist:
+        instance.enqueue(item_to_process)
+    print(item_to_process)
+    
 
 
 def remove(instance):
