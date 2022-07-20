dcbb2d463ea5cc6b59088e4970ca138922e50594
johntvale <johntvale@gmail.com>
2022-07-20 10:51:13 -0400

    Refatoração dos requisitos 6 e 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,7 +1,16 @@
 import re
 
 
-def exists_word(word, instance):
+def get_object(need_content, i, line):
+    if need_content:
+        return {
+            "linha": i + 1,
+            "conteudo": line}
+    else:
+        return {"linha": i + 1}
+
+
+def get_match_data(word, instance, need_content):
     result = []
     word_lower = word.lower()
     for file in instance._data:
@@ -9,11 +18,8 @@ def exists_word(word, instance):
         for i, line in enumerate(file["linhas_do_arquivo"]):
             line_lower = line.lower()
             if re.search(word_lower, line_lower):
-                lines.append(
-                    {
-                        "linha": i + 1,
-                    }
-                )
+                content = get_object(need_content, i, line)
+                lines.append(content)
         if len(lines) > 0:
             result.append(
                 {
@@ -25,26 +31,11 @@ def exists_word(word, instance):
     return result
 
 
+def exists_word(word, instance):
+    result = get_match_data(word, instance, need_content=False)
+    return result
+
+
 def search_by_word(word, instance):
-    result = []
-    word_lower = word.lower()
-    for file in instance._data:
-        lines = []
-        for i, line in enumerate(file["linhas_do_arquivo"]):
-            line_lower = line.lower()
-            if re.search(word_lower, line_lower):
-                lines.append(
-                    {
-                        "linha": i + 1,
-                        "conteudo": line
-                    }
-                )
-        if len(lines) > 0:
-            result.append(
-                {
-                    "palavra": word,
-                    "arquivo": file["nome_do_arquivo"],
-                    "ocorrencias": lines
-                }
-            )
+    result = result = get_match_data(word, instance, need_content=True)
     return result
